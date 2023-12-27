b94ae49c9944bcb3df3f0c73415fea8f201665a4
aryan8255 <97238496+aryan8255@users.noreply.github.com>
2023-12-27 18:29:56 +0530

    Update bot.py

--- AltronX/modules/bot.py
+++ AltronX/modules/bot.py
@@ -20,10 +20,10 @@ from telethon import events
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"» #ɢᴏᴅғᴀᴛʜᴇʀ", parse_mode=None, link_preview=None)
+        altron = await e.reply(f"» #TIGEROP", parse_mode=None, link_preview=None)
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"__🤖 ᴘɪɴɢ__\n» `{mp} ms`")
+        await altron.edit(f"__🗿 ᴘɪɴɢ__\n» `{mp} ms`")
 
 
 @MK1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
@@ -38,7 +38,7 @@ async def ping(e):
 @MK10.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
 async def restart(e):
     if e.sender_id in SUDO_USERS:
-        await e.reply(f"**ᴇʀʀᴏʀ 131**: ꜱᴇʀᴠᴇʀ ɪꜱ ʀᴇꜱᴛᴀʀᴛɪɴɢ 🥵")
+        await e.reply(f"**ᴇʀʀᴏʀ 131**: ꜱᴇʀᴠᴇʀ ɪꜱ ʀᴇꜱᴛᴀʀᴛɪɴɢ 🚩")
         try:
             await MK1.disconnect()
         except Exception:
